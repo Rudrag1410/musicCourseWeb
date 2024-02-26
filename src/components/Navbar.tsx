10c3a44cccd23e4976fa0ebebb2fdebd4d309532
rudrag1410 <rudragupta1410@gmail.com>
2024-02-26 15:05:51 +0530

    Completed the project

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,50 @@
+"use client";
+import React, { useState } from "react";
+import {
+  HoveredLink,
+  Menu,
+  MenuItem,
+  ProductItem,
+} from "@/components/ui/navbar-menu";
+import { cn } from "@/utils/cn";
+import Link from "next/link";
+const Navbar = ({ className }: { className?: string }): JSX.Element => {
+  const [active, setActive] = useState<string | null>(null);
+  return (
+    <div
+      className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}
+    >
+      <Menu setActive={setActive}>
+        <Link href={"/"}>
+          <MenuItem
+            setActive={setActive}
+            active={active}
+            item="Home"
+          ></MenuItem>
+        </Link>
+        <Link href={"/about"}>
+          <MenuItem
+            setActive={setActive}
+            active={active}
+            item="About"
+          ></MenuItem>
+        </Link>
+        <Link href={"/courses"}>
+          <MenuItem
+            setActive={setActive}
+            active={active}
+            item="All Courses"
+          ></MenuItem>
+        </Link>
+        <Link href={"/contact"}>
+          <MenuItem
+            setActive={setActive}
+            active={active}
+            item="Contact Us"
+          ></MenuItem>
+        </Link>
+      </Menu>
+    </div>
+  );
+};
+export default Navbar;
